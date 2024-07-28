037d6ca34ccd4dfd7e1023d851fea48ee70d8351
BlackRain29 <55949931+BlackRain29@users.noreply.github.com>
2024-07-27 23:03:09 -0700

    built out logic /improved the shelf section, added a search bar, and improved design/layout of the shelf section

--- homeBookSys.js
+++ homeBookSys.js
@@ -1,18 +1,13 @@
 // variables
-
-let bookTitle,
-  authorName,
-  genreType,
-  numberOfPages,
-  totalCount,
-  totalCollection,
-  counter = 0,
-  collection = [];
+let counter = 0;
+let collection = [];
+const collectionDisplay = document.getElementById("added_new_book");
 
 // functions
 //add a book
 function addBook() {
-  let newBook;
+  // initialize critical variables
+  let newBook, bookTitle, authorName, genreType, numberOfPages, totalCount;
   bookTitle = document.getElementById("book_title").value;
   authorName = document.getElementById("author").value;
   genreType = document.getElementById("genre_type");
@@ -20,19 +15,21 @@ function addBook() {
   totalCount = document.getElementById("total_count");
 
   //validation check - only all required values entered proceed
-  if (bookTitle && authorName && genreType && numberOfPages) {
-    newBook = {
-      bookTitle: bookTitle,
-      authorName: authorName,
-      genreType: genreType,
-      numberOfPages: numberOfPages,
-    };
+  if (bookTitle && authorName && genreType && !isNaN(numberOfPages)) {
     function growCollection() {
+      // store user inputs in an object
+      newBook = {
+        bookTitle: bookTitle,
+        authorName: authorName,
+        genreType: genreType,
+        numberOfPages: numberOfPages,
+      };
       //structure data as an array of newBook objects
       collection.push(newBook);
       counter++;
       //display count on UI
       totalCount.innerHTML = counter;
+      showCollection();
     }
     function clearInputs() {
       bookTitle = document.getElementById("book_title").value = "";
@@ -40,13 +37,26 @@ function addBook() {
       genreType = document.getElementById("genre_type").value = "";
       numberOfPages = document.getElementById("page_numbers").value = "";
     }
+    growCollection();
+    clearInputs();
   } else {
     alert("Please enter data in all required fields to proceed");
   }
-  growCollection();
-  clearInputs();
 }
 
+//display current collection
+function showCollection() {
+  const bookBox = document.createElement("div");
+  collection.forEach((book) => {
+    bookBox.innerHTML = `<div style="display:block;justify-content:center; align-items:center;" class="shadow-lg rounded px-10 pt-8 pb-10 mb-0 max-w-md mx-4">
+    <p><span style="font-weight: bold; margin-right: 1rem;">Title:</span>${book.bookTitle}</p>
+    <p><span style="font-weight: bold; margin-right: 1rem;">Author:</span>${book.authorName}</p>
+    <p><span style="font-weight: bold; margin-right: 1rem">Genre:</span>${book.genreType.value}</p>
+    <p><span style="font-weight: bold; margin-right: 1rem">Pages:</span>${book.numberOfPages}</p>
+  </div>`;
+  });
+  collectionDisplay.appendChild(bookBox);
+}
 //delete a book
 function removeBook() {}
 
